58808c902ce64b988ad43de322e66501f80cf540
Mike <15527358+devdot4@users.noreply.github.com>
2021-06-26 20:39:41 -0500

    add comments

--- chapter-1/listing-1.1/main.tf
+++ chapter-1/listing-1.1/main.tf
@@ -1,11 +1,15 @@
+# Amazon Web Services
 provider "aws" {
+  # Oregon = us-west-2
   region = "us-west-2"
 }
-
+# AWS Elastic Compute Cloud Instance
 resource "aws_instance" "hello-world" {
+  # ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20200423
   ami           = "ami-09dd2e08d601bff67"
+  # aws free tier instance (burstable)
   instance_type = "t2.micro"
   tags = {
-    Name = "Hello-World"
+    Name = "HelloWorld"
   }
 }
