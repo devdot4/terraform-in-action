96992ad2d6bf2af9e376121cdc67bef64e754700
Mike <15527358+devdot4@users.noreply.github.com>
2021-06-28 17:05:35 -0500

    update code

--- chapter-1/listing-1/1.3/main.tf
+++ chapter-1/listing-1/1.3/main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
   region = "us-west-2"
 }
- 
+
 data "aws_ami" "ubuntu" {
   most_recent = true
- 
+
   filter {
     name   = "name"
-    values = ["ubuntu/01images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
- 
+
   owners = ["099720109477"]
 }
- 
+
 resource "aws_instance" "helloworld" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id 
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
